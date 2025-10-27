955f1c56721b3d57f4eea68840239951adcfe845
Surabhis12 <surabhigowda9986@gmail.com>
2025-10-27 23:49:34 +0530

    Recovered code from test15

--- test-files/javascript/good-example.js
+++ test-files/javascript/good-example.js
@@ -1,14 +1,4 @@
-'use strict';
 
-const Calculator = {
-    result: 0,
-    
-    add: function(a, b) {
-        if (typeof a !== 'number' || typeof b !== 'number') {
-            throw new TypeError('Arguments must be numbers');
-        }
-        this.result = a + b;
-        return this.result;
     },
     
     divide: function(a, b) {
